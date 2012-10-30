17d3fde78bea9db49c9271d4c4a106133e686c8f
karamanov <karamanov@gmail.com>
2012-10-30 10:39:29 +0000

    Refactorred

--- Santase/src/com/karamanov/santase/Santase.java
+++ Santase/src/com/karamanov/santase/Santase.java
@@ -15,37 +15,30 @@ import java.io.ObjectOutputStream;
 import java.io.StreamCorruptedException;
 import java.util.ArrayList;
 
-import com.karamanov.santase.message.MessageProcessor;
-
-import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Environment;
 import android.preference.PreferenceManager;
-import android.util.TypedValue;
 
-public class Santase extends Application {
+import com.karamanov.framework.MessageApplication;
+import com.karamanov.framework.message.MessageType;
+
+public class Santase extends MessageApplication {
+    
+    public final static MessageType MT_KEY_PRESSED = new MessageType("MT_KEY_PRESSED");
+
+    public final static MessageType MT_TOUCH_EVENT = new MessageType("MT_TOUCH_EVENT");
+
+    public final static MessageType MT_EXIT_EVENT = new MessageType("MT_EXIT_EVENT");
+
+    public final static MessageType MT_PAINT_EVENT = new MessageType("MT_PAINT_EVENT");
 
     private static final String SANTASE_DAT = "santase.dat";
     
-    private final MessageProcessor messageProcessor;
-
     private static SantaseFacade santaseFacade;
 
     public Santase() {
         super();
-        messageProcessor = new MessageProcessor();
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        messageProcessor.start();
-    }
-    
-    public final MessageProcessor getMessageProcessor() {
-        return messageProcessor;
     }
 
     public static SantaseFacade getSantaseFacade() {
@@ -140,19 +133,5 @@ public class Santase extends Application {
         } catch (IOException e) {
             // D.N.
         }
-    }
-
-    public static int fromPixelToDip(Context context, int pixels) {
-        Resources resources = context.getResources();
-        if (pixels == 1) {
-            return Math.max(1, Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, pixels, resources.getDisplayMetrics())));
-        } else {
-            return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, pixels, resources.getDisplayMetrics()));
-        }
-    }
-
-    public static float fromPixelToDipF(Context context, float pixels) {
-        Resources resources = context.getResources();
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, pixels, resources.getDisplayMetrics());
     }
 }
