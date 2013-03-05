c318e225f4c59c496de7feca27afb35676f62850
karamanov@gmail.com <karamanov@gmail.com@6e8867f1-d30b-aee2-9515-98d9af3a9b61>
2013-03-05 14:59:29 +0000

    Refactorred

--- Santase/src/game/logic/strategy/validator/OrdinaryGameValidator.java
+++ Santase/src/game/logic/strategy/validator/OrdinaryGameValidator.java
@@ -4,7 +4,7 @@ import game.beans.Player;
 import game.beans.pack.card.Card;
 import game.beans.pack.card.suit.Suit;
 
-public class OrdinaryGameValidator extends BasicGameValidator {
+public final class OrdinaryGameValidator extends BasicGameValidator {
 
     @Override
     public ValidateCode validatePlayerCard(Player player, Card ownCard, Card rivalCard, Suit trumpSuit) {
